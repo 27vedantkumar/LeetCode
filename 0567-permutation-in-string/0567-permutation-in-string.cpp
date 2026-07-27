763268cfa2d6d244d4c32af46f79e5169580e62d
Vedant-kumar-27 <vedant.kumar2025@lpu.in>
2026-07-27 14:51:55 +0530

    Time: 199 ms (20.7%), Space: 10 MB (70.85%) - LeetHub

---
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool isFreqSame(int freq1[],int freq2[]) {
+        for(int i=0;i<26;i++) {
+            if(freq1[i]!=freq2[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+    bool checkInclusion(string s1, string s2) {
+        int freq[26]={0};
+        for(int i=0;i<s1.length();i++) {
+            freq[s1[i]-'a']++;
+        }
+        int windowSize=s1.length();
+        for(int i=0;i<s2.length();i++) {
+            int windowIndex=0,index=i;
+            int windowFreq[26]={0};
+            while(windowIndex<windowSize && index<s2.length()) {
+                windowFreq[s2[index]-'a']++;
+                windowIndex++;
+                index++;
+            }
+            if(isFreqSame(freq,windowFreq)) return true;
+        }
+        return false;
+    }
+};

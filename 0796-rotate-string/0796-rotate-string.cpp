e506d9aadae5c1d4bcb6557a1ebb981ab73a9faa
Vedant-kumar-27 <vedant.kumar2025@lpu.in>
2026-07-28 02:23:49 +0530

    Time: 0 ms (100%), Space: 8.2 MB (20.21%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.cpp
+++ 0796-rotate-string/0796-rotate-string.cpp
@@ -3,9 +3,7 @@ public:
     bool rotateString(string s, string goal) {
         string ss=s+s;
         if(s.length()!=goal.length()) return false;
-        for(int i=0;i<ss.length();i++) {
             if(ss.contains(goal)) return true;
-        }
         return false;
     }
 };

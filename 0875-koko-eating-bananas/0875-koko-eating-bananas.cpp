ab35434cd53d92d4a8ebd8c90d044d0b6bcb1e86
Vedant-kumar-27 <vedant.kumar2025@lpu.in>
2026-08-01 23:47:15 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int findMax(vector<int> &v) {
+        int maxi=INT_MIN;
+        int n=v.size();
+        for(int i=0;i<n;i++) {
+            maxi=max(maxi,v[i]);
+        }
+        return maxi;
+    }
+    int calculateTotalHours(vector<int> &v,int hour) {
+        long long totalH=0;
+        int n=v.size();
+        for(int i=0;i<n;i++) {
+            totalH +=ceil((double)v[i]/(double)hour);
+        }
+        return totalH;
+    }
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int low=1,high=findMax(piles);
+        while(low<=high) {
+            int mid=(low+high)/2;
+            int totalH=calculateTotalHours(piles,mid);
+            if(totalH<=h) high=mid-1;
+            else low=mid+1;
+        }
+        return low;
+    }
+};

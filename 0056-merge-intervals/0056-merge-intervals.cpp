295f583755145a7267814eb2fb33d5889ded27de
KINETICxSOUL <27vedantkumar@gmail.com>
2026-08-19 19:50:46 +0530

    Time: 8 ms (23.93%), Space: 23.7 MB (82.58%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -8,9 +8,7 @@ public:
             if( ans.empty() || intervals[i][0]>ans.back()[1]) {
                 ans.push_back(intervals[i]);
             }
-            else {
-                ans.back()[1]=max(ans.back()[1], intervals[i][1]);
-            }
+            else ans.back()[1]=max(ans.back()[1], intervals[i][1]);
         }
         return ans;
     }
